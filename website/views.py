f1b0f6f517b1417642e69769d2c80bf73c0334b5
masooma <masoomashah05@gmail.com>
2025-05-10 09:52:51 +0500

    editing?

--- website/views.py
+++ website/views.py
@@ -3,8 +3,9 @@ from flask_login import login_required,current_user
 from .models import Note
 from . import db
 import psycopg2
-from config import fetch_data,Delete_rows
+from config import fetch_data
 import json
+import datetime as datetime
 views=Blueprint('views',__name__)
 
 @views.route('/')
@@ -18,7 +19,7 @@ def home():
     return render_template("home.html",columns=columns,rows=rows)
 
 
-@views.route('/delete-records', methods=['POST'])
+# @views.route('/delete-records', methods=['POST'])
     
 # def delete_records():
 #     delete_rows()
@@ -28,11 +29,43 @@ def home():
 #     Delete_rows()
 #     return redirect(url_for('views.home') )
 
-def delete_records():
-      # Gets all selected checkboxes
-    Delete_rows()
+# def delete_records():
+#       # Gets all selected checkboxes
+#     Delete_rows()
 
 
-    return redirect(url_for('views.home'))
+#     return redirect(url_for('views.home'))
+
+@views.route('/handle_records', methods=['POST'])
+def handle_records():
+    print("HANDLE_RECORDS CALLED")
+
+    flash(f"TEST: {datetime.now()}")
+    return render_template('login.html')
+    # if not selected_ids:
+    #     flash('Please select at least one record.')
+    #     return redirect(url_for('views.home'))
+
+                # if action == 'delete':
+                #     Delete_rows(selected_ids)
 
 
+                # elif action == 'edit':
+                #     flash("please work")
+
+         
+        # return redirect(url_for('views.test_flash'))
+        # if len(selected_ids) != 1:
+        #     flash('Please select exactly one record to edit.')
+        #     return redirect(url_for('views.home'))
+        # record_id = selected_ids[0]
+        # return redirect(url_for('views.Edit_record'))
+
+    # else:
+    #     flash('Unknown action.')
+    #     return redirect(url_for('views.home'))
+
+@views.route('/Edit_record', methods=['GET', 'POST'])
+def Edit_record():
+    flash("you just clicked the edit button whaaaaaaaa")
+    return render_template('login.html')
